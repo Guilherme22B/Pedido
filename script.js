aaad5782db731ec0118fc69dd5c4a57d02e1b6b2
GuilhermeSilvaM <guimenezes125@gmail.com>
2023-11-29 11:40:23 -0300

    pedido01

---
+++ script.js
@@ -0,0 +1,25 @@
+function numeroAleatorio(min, max){
+    return Math.random() * (max + min) + '%';
+}
+
+
+function desvia(t){
+    var btn = t
+
+    btn.style.position = 'absolute'
+    btn.style.bottom = numeroAleatorio(1, 85)
+    btn.style.left = numeroAleatorio(1,85)
+
+
+}
+
+
+function sim(){
+    var a = document.querySelector('button')
+    var b = document.querySelector('button#btn')
+    var c = document.querySelector('img')
+
+    a.style.display = 'none'
+    b.style.display = 'none'
+    c.style.display = 'block'
+}
