82a230a96f0c3bb0a88a292e7790a81767c65815
GuilhermeSilvaM <guimenezes125@gmail.com>
2023-12-06 15:23:39 -0300

    .

--- script.js
+++ script.js
@@ -4,28 +4,28 @@ function numeroAleatorio(min, max){
 
 
 function desvia(t){
-    var des = document.querySelector("h1")
-
-    des.innerHTML = 'DESCULPA, EU ACABEI ME QUEIMANDO DE GRAÇA COM UMA PESSOA INCRIVEL'
-
-
-    /*
     var btn = t
 
     btn.style.position = 'absolute'
     btn.style.bottom = numeroAleatorio(1, 85)
     btn.style.left = numeroAleatorio(1,85)
 
-*/
 }
 
 
 function sim(){
+    var des = document.querySelector("h1")
+
+    des.innerHTML = 'DESCULPA, EU FUI BURRO E VACILEI PARA KRL'
+
+/*
     var a = document.querySelector('button')
     var b = document.querySelector('button#btn')
     var c = document.querySelector('img')
 
+
     a.style.display = 'none'
     b.style.display = 'none'
     c.style.display = 'block'
+    */
 }
